243de00d587176dd3f954bacab126c8b1acb2633
Hassan Sambo <sambohasan@gmail.com>
2024-01-23 17:49:54 +0100

    feat: add database models and schema migration

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
